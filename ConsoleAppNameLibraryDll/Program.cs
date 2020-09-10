202ed3a0e1a8a420c4ac4a3cefe3814d35a84dd8
DonCalzoneDK <58050674+DonCalzoneDK@users.noreply.github.com>
2020-09-10 10:29:34 +0200

    Add project files.

---
+++ ConsoleAppNameLibraryDll/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http.Headers;
+using ClassLibraryName;
+
+namespace ConsoleAppNameLibraryDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ClassLibraryName.Name name = new ClassLibraryName.Name("Josh");
+
+            Console.WriteLine(name.MyNameIs());
+            
+        }
+    }
+}
